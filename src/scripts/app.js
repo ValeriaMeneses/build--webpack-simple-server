c425232f7b66a0f5df9c42c5bfdbd9cd8d31b7a9
valeria <valeriairaismeneses@gmail.com>
2017-10-23 11:53:40 -0500

    completo

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,32 +5,32 @@ import allCountries from './templateViews/allCountries.js'
 
 const containerCountries = document.querySelector('.countries-container')
 
-function changeBackground() {
-  let links = document.querySelectorAll('.botones a')
-  for (var i = 0; i < links.length; i++) {
-    links[i].addEventListener('click', function() {
-        for (var i = 0; i < links.length; i++) {
-          links[i].classList.remove('background-blue');
-        };
-        this.className += 'background-blue';
-    });
-    }
-}
-changeBackground();
-
-function showCountries() {
-  request
-    .get('https://restcountries.eu/rest/v2/all')
-    .then(function (data) {
-      let obj = data.body
-      return obj
-    })
-    .then(function (obj) {
-        obj.forEach(function (element) {
-          containerCountries.innerHTML += allCountries(element)
-        })
-    })
+  function changeBackground() {
+    let links = document.querySelectorAll('.botones a')
+    for (var i = 0; i < links.length; i++) {
+      links[i].addEventListener('click', function() {
+          for (var i = 0; i < links.length; i++) {
+            links[i].classList.remove('background-blue');
+          };
+          this.className += 'background-blue';
+      });
+      }
   }
+  changeBackground();
+
+  function showCountries() {
+    request
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(function (data) {
+        let obj = data.body
+        return obj
+      })
+      .then(function (obj) {
+          obj.forEach(function (element) {
+            containerCountries.innerHTML += allCountries(element)
+          })
+      })
+    }
 
   function showCountriesByLanguage(lang) {
     // console.log(lang);
@@ -55,7 +55,7 @@ function showCountries() {
   const routes = {
   '/all': showCountries,
   '/languages/:id': showCountriesByLanguage
-};
+  };
 
 const router = Router(routes);
 router.init('/');
